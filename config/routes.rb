d9e5b8735221130002949c403fca675856ec903b
Demi007 <gorigoriuhouho0820@gmail.com>
2021-05-15 11:16:49 +0000

    bootstrap追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/index'
-  get 'users/edit'
   root to: 'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: [:create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
 end
