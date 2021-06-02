5860b7efd0e5641740284bebe68bdc651e9b344e
Demi007 <gorigoriuhouho0820@gmail.com>
2021-06-02 03:50:25 +0000

    いいね機能実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => "homes#about"
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users
+
 end
