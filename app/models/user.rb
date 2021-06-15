e362afcce1478018cd11382233c6f60fa55cd013
Demi007 <gorigoriuhouho0820@gmail.com>
2021-06-15 14:27:32 +0000

    dm機能model,conntroller追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
   attachment :profile_image
   validates :name, presence: true, uniqueness: true, length: {minimum:2, maximum:20 }
   validates :introduction, length: {maximum:50}
