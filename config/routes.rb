d97e1d607456fe6cbc1767dcae8a36909256ce9c
Demi007 <gorigoriuhouho0820@gmail.com>
2021-06-15 15:40:56 +0000

    DM機能実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => "homes#about"
   get 'search' => 'searches#search'
+  resources :users, only: [:show,:edit,:update]
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
